0038b33e9f5135d4c7c9a755e66d7b2c3d625ac4
Karl van Heijster <Karl.vanHeijster@cito.nl>
2021-04-23 19:43:33 +0200

    Don't introduce Factory untill V03

--- RefactorExercises/EnumSwitch/Refactored/V02/ClaimsHelper.cs
+++ RefactorExercises/EnumSwitch/Refactored/V02/ClaimsHelper.cs
@@ -1,4 +1,5 @@
 ﻿using RefactorExercises.EnumSwitch.Model;
+using System;
 using System.Text;
 
 namespace RefactorExercises.EnumSwitch.Refactored.V02
@@ -19,12 +20,23 @@ namespace RefactorExercises.EnumSwitch.Refactored.V02
             // User can have multiple claims, so loop through them
             foreach (var permission in _user.Permissions.ToEnumerable())
             {
-                var claimGetter = ClaimFactory.GetClaim(permission);
+                var claimGetter = GetClaim(permission);
                 claimsBuilder.AppendLine(claimGetter.GetClaim());
             }
 
             // Return all the claims for the User
             return claimsBuilder.ToString();
+        }
+
+        private IGetClaim GetClaim(Permission permission)
+        {
+            return permission switch
+            {
+                Permission.Read => new GetReadClaim(),
+                Permission.Write => new GetWriteClaim(),
+                Permission.Delete => new GetDeleteClaim(),
+                _ => throw new NotSupportedException($"Claim of type '{permission}' is not supported"),
+            };
         }
     }
 }
